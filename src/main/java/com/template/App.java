dc41dcaff81cfa19c9ec2b3792716cf4207dc89c
Nicolas-GonzalezPiskulic <you@example.comngonzalez2@mail.austral.edu.ar>
2025-08-22 13:01:49 -0300

    minor changes

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -99,66 +99,111 @@ public class App {
     // 11. Sum of Elements in a List
     public int sumList(List<Integer> list) {
         // TODO: Implement this method
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            sum += list.get(i);
+        }
+        return(sum);
     }
 
     // 12. Find the Largest Element in a List
     public int findMax(List<Integer> list) {
         // TODO: Implement this method
-        return 0;
+        if (!(list.isEmpty())) {
+            int max = list.getFirst();
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i) > max) {
+                    max = list.get(i);
+                }
+            }
+            return max;
+        }
+        else {
+            throw new IllegalArgumentException("list is empty");
+        }
     }
 
     // 13. Filter Even Numbers from a List
     public List<Integer> filterEvenNumbers(List<Integer> list) {
         // TODO: Implement this method
-        return new ArrayList<>();
+        List<Integer> evens = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if ((list.get(i)%2)==0){
+                evens.add(list.get(i));
+            }
+        }
+        return evens;
     }
 
     // 14. Concatenate Two Lists
     public List<String> concatenateLists(List<String> list1, List<String> list2) {
         // TODO: Implement this method
-        return new ArrayList<>();
+        List<String> lists = new ArrayList<>();
+        for (int i = 0; i < list1.size(); i++) {
+            lists.add(list1.get(i));
+        }
+        for (int i = 0; i < list2.size(); i++) {
+            lists.add(list2.get(i));
+        }
+        return lists;
     }
 
     // 15. Check if List Contains Element
     public boolean listContains(List<String> list, String element) {
         // TODO: Implement this method
-        return false;
+        return (list.contains(element));
     }
 
     // 16. Convert Strings to Uppercase
     public List<String> toUpperCase(List<String> list) {
         // TODO: Implement this method
-        return new ArrayList<>();
+        for  (int i = 0; i < list.size(); i++) {
+            list.set(i, list.get(i).toUpperCase());
+        }
+        return list;
     }
 
     // 17. Remove Duplicates from a List
     public List<Integer> removeDuplicates(List<Integer> list) {
         // TODO: Implement this method
-        return new ArrayList<>();
+        List<Integer> nodup = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+                if (!(nodup.contains(list.get(i)))){
+                    nodup.add(list.get(i));
+            }
+        }
+        return nodup;
     }
 
     // 18. Convert List to Set for Unique Elements
     public Set<Integer> listToSet(List<Integer> list) {
         // TODO: Implement this method
-        return new HashSet<>();
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < list.size(); i++) {
+            set.add(list.get(i));
+        }
+        return set;
     }
 
     // 19. Check if Map Contains Key
     public boolean mapContainsKey(Map<String, String> map, String key) {
         // TODO: Implement this method
-        return false;
+        return map.containsKey(key);
     }
 
     // 20. Check if Map Contains Value
     public boolean mapContainsValue(Map<String, String> map, String value) {
         // TODO: Implement this method
-        return false;
+        return map.containsValue(value);
     }
 
     // 21. Iterate Over a Map
     public List<String> iterateMap(Map<String, String> map) {
         // TODO: Implement this method
-        return new ArrayList<>();
+        List<String> resultList = new ArrayList<>();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            resultList.add(entry.getKey() + " -> " + entry.getValue());
+        }
+        return resultList;
     }
 }
